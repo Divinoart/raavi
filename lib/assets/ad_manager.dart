5f9cc4ebd14af9a03c8539bc57396f49b0bd05d9
Divine <anakor.d.13@gmail.com>
2021-02-16 08:54:32 +0100

    test run

--- lib/assets/ad_manager.dart
+++ lib/assets/ad_manager.dart
@@ -1,6 +1,39 @@
 import 'dart:io';
 
 class AdManager {
+
+  // static String get appId {
+  //   if (Platform.isAndroid) {
+  //     return "ca-app-pub-3940256099942544~3347511713";
+  //   } else if (Platform.isIOS) {
+  //     return "ca-app-pub-3940256099942544~1458002511";
+  //   } else {
+  //     throw new UnsupportedError("Unsupported platform");
+  //   }
+  // }
+  //
+  // static String get bannerAdUnitId {
+  //   if (Platform.isAndroid) {
+  //     return "ca-app-pub-3940256099942544/6300978111";
+  //   } else if (Platform.isIOS) {
+  //     return "ca-app-pub-3940256099942544/2934735716";
+  //   } else {
+  //     throw new UnsupportedError("Unsupported platform");
+  //   }
+  // }
+  //
+  // static String get interstitialAdUnitId {
+  //   if (Platform.isAndroid) {
+  //     return "ca-app-pub-3940256099942544/1033173712";
+  //   } else if (Platform.isIOS) {
+  //     return "ca-app-pub-3940256099942544/4411468910";
+  //   } else {
+  //     throw new UnsupportedError("Unsupported platform");
+  //   }
+  // }
+
+
+  //todo: live ID
   static String get appId {
     if (Platform.isAndroid) {
       return "ca-app-pub-1396258455588539~2849139983";
